1988df120384726eb729b46a0788d44afd026cf5
Rsilgal <rsilgal@gmail.com>
2022-03-27 21:10:10 +0200

    Challenge 2. Completed, second version without slices

--- Golang/Week 2/main.go
+++ Golang/Week 2/main.go
@@ -2,46 +2,28 @@ package main
 
 import "fmt"
 
-func calcFibonacci(listInput []int, numOfElements int) []int {
-	list := listInput
-	for i := 1; i <= numOfElements-2; i++ {
-		list = append(list, list[i-1]+list[i])
+/*
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+*/
+
+func calcFib(numOfElements int) {
+	f1 := 0
+	f2 := 1
+
+	for i := 1; i <= numOfElements; i++ {
+		printFibonacci(f1, i == numOfElements)
+		f1, f2 = f2, (f1 + f2)
 	}
-	return list
 }
 
-func printSlice(list []int) {
-	numOfElemnts := len(list) - 1
-	for index, elem := range list {
-		if index == numOfElemnts {
-			fmt.Printf("%d", elem)
-		} else {
-			fmt.Printf("%d, ", elem)
-		}
+func printFibonacci(num int, isLastElem bool) {
+	if isLastElem {
+		fmt.Printf("%d", num)
+		return
 	}
+	fmt.Printf("%d, ", num)
 }
 
 func main() {
-	var fib []int
-	fib = append(fib, 0)
-	fib = append(fib, 1)
-
-	list := calcFibonacci(fib, 50)
-	printSlice(list)
+	calcFib(50)
 }
-
-/*
-0
-1
-1
-2
-3
-5
-8
-13
-21
-34
-55
-89
-...
-*/
