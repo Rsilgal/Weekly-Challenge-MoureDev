d41faa4ca231ee00b0224f50d849c425bd9f5a40
Rsilgal <rsilgal@gmail.com>
2022-04-02 17:31:03 +0200

    Define countDiferentWords function

--- Golang/Week 7/main.go
+++ Golang/Week 7/main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"regexp"
 )
 
-func formatText(text string) string {
-	return ""
+func formatText(text string, re *regexp.Regexp) []string {
+	return re.Split(text, -1)
 }
 
 func countDiferentsWords() map[string]int {
+	counter := make(map[string]int)
+
+	
 	return map[string]int{}
 }
 
+func buildRegex() *regexp.Regexp {
+	return regexp.MustCompile(`(,\s)|(\.*\.\s)|(\s)|(\.)$`) 
+}
+
 func main() {
-	// regex split
-	regexp.MustCompile(`[.,\/#!$%\^&\*;:{}=\-_~()”“"…\s]`)
+	re := buildRegex()
+
+	text := "Hola, esto es un texto bastante aleatorio."
+
+	a := formatText(text, re)
+
+	for _, c := range a {
+		fmt.Println(c)
+	}
 }
